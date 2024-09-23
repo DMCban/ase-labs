0a5c675af19be8893dfdbf2af96b5c3b5af15689
aselab2b <ban.odett@edu.bme.hu>
2024-09-23 17:48:19 +0200

    lab2

---
+++ similarity/build.gradle.kts
@@ -0,0 +1,25 @@
+
+plugins {
+    id("hu.bme.mit.ase.shingler.gradle.application")
+
+}
+
+dependencies {
+
+    api(project(":lib"))
+    api(project(":logic"))
+
+    implementation(libs.slf4j.api)
+    implementation("info.picocli:picocli:$picoCliVersion")
+
+    runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+}
+
+
+application {
+    mainClass = "hu.bme.mit.ase.shingler.similarity.SimilarityApp"
+}
